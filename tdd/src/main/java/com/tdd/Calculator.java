6183870d088ebf193f9f59c044be805cd898c8a0
Thomas Green <thomas.green@ithaka.org>
2016-07-22 09:30:15 -0400

    TDD KATA

--- tdd/src/main/java/com/tdd/Calculator.java
+++ tdd/src/main/java/com/tdd/Calculator.java
@@ -1,9 +1,41 @@
 package com.tdd;
 
+import org.springframework.util.StringUtils;
+
 /**
  * Created by tgreen on 7/12/16.
  */
 public class Calculator {
+
+
+    public int add(String s) {
+        if (!StringUtils.isEmpty(s)){
+            String delimiter;
+            if (s.startsWith("//")){
+                delimiter = s.split("\n")[0].substring(2);
+                s = s.replace("//"+delimiter+"\n", "");
+            }
+            else {
+                delimiter = ",|\n";
+            }
+            String[] strings = s.split(delimiter);
+            int sum = getSum(strings);
+            return sum;
+        }
+        return 0;
+    }
+
+    private int getSum(String[] strings) {
+        int sum =0;
+        for (int i=0; i<strings.length; i++){
+            int integer = Integer.parseInt(strings[i]);
+            if (integer < 0 ){
+                throw new NumberFormatException("negatives not allowed");
+            }
+            sum += integer;
+        }
+        return sum;
+    }
 
 
 }
--- tdd/src/test/java/com/tdd/CalculatorTest.java
+++ tdd/src/test/java/com/tdd/CalculatorTest.java
@@ -1,11 +1,83 @@
 package com.tdd;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created by tgreen on 7/12/16.
  */
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void add_emptyString_returnsZero(){
+        int actual = calculator.add("");
+
+        assertThat(actual, is(0));
+    }
+
+    @Test
+    public void add_nullString_returnsZero(){
+        int actual = calculator.add(null);
+
+        assertThat(actual, is(0));
+    }
+
+    @Test
+    public void add_oneNumber_returnsNumber(){
+        int actual = calculator.add("1");
+
+        assertThat(actual, is(1));
+    }
+
+    @Test
+    public void add_twoNumbers_returnsSum(){
+        int actual = calculator.add("1,2");
+
+        assertThat(actual, is(3));
+    }
+
+    @Test
+    public void add_fourNumbers_returnsSum(){
+        int actual = calculator.add("1,2,3,4");
+
+        assertThat(actual, is(10));
+    }
+
+    @Test
+    public void add_lineBreakDelimiter_returnsSum(){
+        int actual = calculator.add("1\n2,3");
+
+        assertThat(actual, is(6));
+    }
+
+    @Test
+    public void add_defineDelimiter_returnsSum(){
+        int actual = calculator.add("//;\n1;2");
+
+        assertThat(actual, is(3));
+    }
+
+    @Test
+    public void add_negativeNumber_NumberFormatExceptionWithMessage(){
+        thrown.expect(NumberFormatException.class);
+        thrown.expectMessage("negatives not allowed");
+
+        calculator.add("1,-2");
+    }
 
 }
