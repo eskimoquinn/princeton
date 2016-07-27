9232d8bc2146716a279798c3202277a62eec6f33
Thomas Green <thomas.green@ithaka.org>
2016-07-27 15:13:16 -0400

    Adding Watchables Demo

---
+++ watchablesdemo/src/main/java/org/watchablesdemo/WatchablesdemoApplication.java
@@ -0,0 +1,12 @@
+package org.watchablesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchablesdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchablesdemoApplication.class, args);
+	}
+}
